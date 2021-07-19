ca56c9218e8d66de8efcb4e720ce46c54bc40699
Julia Moroshchuk <56607318+juliamoro@users.noreply.github.com>
2021-07-19 21:21:38 +0300

    Add files via upload

---
+++ src/by/academy/homework2/hwork3.java
@@ -0,0 +1,25 @@
+package by.academy.homework;
+
+import java.util.Scanner;
+
+public class hwork3 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        boolean n = true;
+        while(n) {
+            String s1 = scanner.nextLine();
+            String s2 = scanner.nextLine();
+            if(s1.length() % 2 == 0 & s2.length() % 2 == 0 & !s1.isEmpty() & !s2.isEmpty()){
+                StringBuilder str1 = new StringBuilder(s1);
+                StringBuilder str2 = new StringBuilder(s2);
+                int tempS1 = s1.length()/2;
+                str1.delete(tempS1,s1.length());
+                int tempS2 = s2.length()/2;
+                str2.delete(0,tempS2);
+                str1.append(str2);
+                System.out.println(str1);
+                n = false;
+            }
+        }
+    }
+}
